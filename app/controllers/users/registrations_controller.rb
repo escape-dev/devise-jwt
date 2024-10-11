a32089731a52f0017bc5e0ff448bfe4579f19755
hafiz <hafiz170301@gmail.com>
2024-10-11 16:42:46 +0700

    feat: create endpoint to create session and create new user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
   respond_to :json
+
+  private 
+
+  def respond_with(resource, _opts = {})
+    if request.method == "POST" && resource.persisted?
+      render json: { message: "OK", data: resource }, status: :created
+    elsif request.method == "DELETE"
+      render json: { message: "OK", data: resource }, status: :ok 
+    else
+      render json: { message: "Error", data: resource.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
   
 end
