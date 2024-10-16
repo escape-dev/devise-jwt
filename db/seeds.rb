018438ae637ba3ec78a704cefb1c377d41620f35
Muhammad Hafiz <hafiz170301@gmail.com>
2024-10-16 16:36:47 +0700

    feat: create seeder and adjust user relation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.find_or_create_by(email: "admin@gmail.com") do |u|
+  u.role = 0
+  u.password = "password123"
+  u.password_confirmation = "password123"
+end
+
+books = [
+  { title: "Solo Leveling", description: "Wage War", user_id: user.id },
+  { title: "Solo Leveling: Origin", description: "Wage War", user_id: user.id }
+]
+
+books.each { |book| Book.find_or_create_by(book) }
