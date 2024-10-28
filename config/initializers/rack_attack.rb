f57c88e30e7a806443da0fca1c638c7473edad2a
Muhammad Hafiz <hafiz170301@gmail.com>
2024-10-28 15:51:14 +0700

    feat: adding protection againts spammer

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,31 @@
+class Rack::Attack
+
+  self.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  throttle('default protector', limit: 300, period: 5.minutes) do |req|
+    req.ip 
+  end
+
+  throttle('login protector', limit: 5, period: 20.seconds) do |req|
+    if req.path == '/login' && req.post?
+      req.ip
+    end
+  end
+
+  self.throttled_responder = lambda do |request|
+    match_data = request.env['rack.attack.match_data']
+    now = match_data[:epoch_time]
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'RateLimit-Limit' => match_data[:limit].to_s,
+      'RateLimit-Remaining' => '0',
+      'RateLimit-Reset' => (now + (match_data[:period] - now % match_data[:period])).to_s
+    }
+
+    body = { 'message' => "oh god, what do you want?" }.to_json
+
+    [ 429, headers, [body]]
+  end
+
+end
